5fe6fccc4c6adf0e0f8cc0baf79a339602f542db
cnjameshu <jameshu@live.com>
2025-12-03 02:08:53 +0800

    update

--- api.py
+++ api.py
@@ -334,4 +334,4 @@ async def get_video(video_path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
